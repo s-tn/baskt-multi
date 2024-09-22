86919ded7c34a134c2ff87800634d4327153e70c
jsbn <78042486+s-tn@users.noreply.github.com>
2024-09-22 14:20:49 -0400

    more args

--- headless.js
+++ headless.js
@@ -12,7 +12,7 @@ const run = async () => {
 
     const browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--no-zygote', '--disable-gpu'],
         executablePath: chromiumExec,
     });
     const page = await browser.newPage({
